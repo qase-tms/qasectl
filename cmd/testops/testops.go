21320371e1648a0169f2aef7d167a2a6c94eca35
Dmitrii Gridnev <dgridnev@qase.io>
2024-05-13 13:39:26 +0200

    Added `testops` parent command for all operations with Qase.

---
+++ cmd/testops/testops.go
@@ -0,0 +1,47 @@
+package testops
+
+import (
+	"fmt"
+	"github.com/qase-tms/qasectl/cmd/flags"
+	"github.com/qase-tms/qasectl/cmd/testops/run"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	tokenFlag   = "token"
+	projectFlag = "project"
+)
+
+// Command returns a new cobra command for testops
+func Command() *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:   "testops",
+		Short: "Manage test operations",
+	}
+
+	cmd.PersistentFlags().StringP(tokenFlag, "t", "", "token for Qase API access")
+	err := viper.BindPFlag(flags.TokenFlag, cmd.PersistentFlags().Lookup(tokenFlag))
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = cmd.MarkPersistentFlagRequired(tokenFlag)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	cmd.PersistentFlags().StringP(projectFlag, "p", "", "project code for Qase API access")
+	err = viper.BindPFlag(flags.ProjectFlag, cmd.PersistentFlags().Lookup(projectFlag))
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = cmd.MarkPersistentFlagRequired(projectFlag)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	cmd.AddCommand(run.Command())
+
+	return cmd
+}
