35e9225596aefc41ed5bb4eb9ae3abe155b3a08e
Dmitrii Gridnev <dgridnev@qase.io>
2024-05-16 12:20:02 +0200

    Added slog logger
    Added slog logger and verbose parameter to all command. The parameter has two forms: `--verbose` and `-v`.

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,8 @@ import (
 	"github.com/qase-tms/qasectl/cmd/version"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"log/slog"
+	"os"
 )
 
 var rootCmd = &cobra.Command{
@@ -17,9 +19,32 @@ func Execute() {
 }
 
 func init() {
+	cobra.OnInitialize(setLogger)
 	viper.SetEnvPrefix("QASE_TESTOPS")
 	viper.AutomaticEnv()
 
+	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "verbose output")
+	err := viper.BindPFlag("DEBUG", rootCmd.PersistentFlags().Lookup("verbose"))
+	if err != nil {
+		slog.Error("failed to bind flag", "flag", "verbose", "error", err)
+	}
+
 	rootCmd.AddCommand(testops.Command())
 	rootCmd.AddCommand(version.VersionCmd())
+}
+
+func setLogger() {
+	debug := viper.GetBool("Debug")
+
+	ll := slog.LevelInfo
+	if debug {
+		ll = slog.LevelDebug
+	}
+	opts := &slog.HandlerOptions{
+		Level: ll,
+	}
+
+	handler := slog.NewTextHandler(os.Stdout, opts)
+
+	slog.SetDefault(slog.New(handler))
 }
