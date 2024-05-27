95998681d199c65d97844aa387fba3fe6f2ecfc6
Dmitrii Gridnev <dgridnev@qase.io>
2024-05-27 16:39:46 +0200

    run: add support environment
    - add a new flag `environment`
    - update internal logic of the run service

--- internal/service/run/run.go
+++ internal/service/run/run.go
@@ -1,10 +1,15 @@
 package run
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"github.com/qase-tms/qasectl/internal/models/run"
+)
 
 // client is a client for run
 type client interface {
-	CreateRun(ctx context.Context, projectCode, title string, description *string) (int64, error)
+	GetEnvironments(ctx context.Context, projectCode string) ([]run.Environment, error)
+	CreateRun(ctx context.Context, projectCode, title string, description string, envID int64) (int64, error)
 	CompleteRun(ctx context.Context, projectCode string, runId int64) error
 }
 
@@ -19,8 +24,21 @@ func NewService(client client) *Service {
 }
 
 // CreateRun creates a new run
-func (s *Service) CreateRun(ctx context.Context, projectCode, title string, description *string) (int64, error) {
-	return s.client.CreateRun(ctx, projectCode, title, description)
+func (s *Service) CreateRun(ctx context.Context, p, t string, d, e string) (int64, error) {
+	var envID int64 = 0
+	if e != "" {
+		es, err := s.client.GetEnvironments(ctx, p)
+		if err != nil {
+			return 0, fmt.Errorf("failed to get environments: %w", err)
+		}
+		for _, env := range es {
+			if env.Slug == e {
+				envID = env.ID
+			}
+		}
+	}
+
+	return s.client.CreateRun(ctx, p, t, d, envID)
 }
 
 // CompleteRun completes a run
